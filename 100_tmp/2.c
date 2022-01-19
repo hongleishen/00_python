7de6178d5a6e4bc0a2765ddc8e743f0128858828
shenhonglei <shladi@126.com>
2022-01-20 01:56:27 +0800

    struct ok at home
    Signed-off-by: shenhonglei <shladi@126.com>

--- 100_tmp/2.c
+++ 100_tmp/2.c
@@ -1,4 +1,67 @@
+struct usb_gadget_driver {
+	char            *function;
+	enum usb_device_speed   max_speed;
+	int         (*bind)(struct usb_gadget *gadget,
+					struct usb_gadget_driver *driver);
+	void            (*unbind)(struct usb_gadget *);
+	int         (*setup)(struct usb_gadget *,
+					const struct usb_ctrlrequest *);
+	void            (*disconnect)(struct usb_gadget *);
+	void            (*suspend)(struct usb_gadget *);
+	void            (*resume)(struct usb_gadget *);
+	void            (*reset)(struct usb_gadget *);
+	char            *udc_name;
+	struct list_head    pending;
+	unsigned                match_existing_only:1;
+};
 
-struct test {
-    int a;
+
+struct musb_fifo_cfg {
+	u8          hw_ep_num;
+	enum musb_fifo_style    style;
+	enum musb_buf_mode  mode;
+	u16         maxpacket;
+};
+
+
+struct musb_fifo_cfg {
+	u8          hw_ep_num;
+	enum musb_fifo_style    style;
+	enum musb_buf_mode  mode;
+	u16         maxpacket;
+};
+
+
+struct musb_hdrc_eps_bits {
+	const char  name[16];
+	u8      bits;
+};
+
+
+struct musb_hdrc_config {
+	struct musb_fifo_cfg    *fifo_cfg;  /* board fifo configuration */
+	unsigned        fifo_cfg_size;  /* size of the fifo configuration */
+
+	/* MUSB configuration-specific details */
+	unsigned    multipoint:1;   /* multipoint device */
+	unsigned    dyn_fifo:1; /* supports dynamic fifo sizing */
+	unsigned    soft_con:1; /* soft connect required */
+	unsigned    utm_16:1; /* utm data witdh is 16 bits */
+	unsigned    big_endian:1;   /* true if CPU uses big-endian */
+	unsigned    mult_bulk_tx:1; /* Tx ep required for multbulk pkts */
+	unsigned    mult_bulk_rx:1; /* Rx ep required for multbulk pkts */
+	unsigned    high_iso_tx:1;  /* Tx ep required for HB iso */
+	unsigned    high_iso_rx:1;  /* Rx ep required for HD iso */
+	unsigned    dma:1; /* supports DMA */
+	unsigned    vendor_req:1; /* vendor registers required */
+
+	u8      num_eps;    /* number of endpoints _with_ ep0 */
+	u8      dma_channels; /* number of dma channels */
+	u8      dyn_fifo_size;  /* dynamic size in bytes */
+	u8      vendor_ctrl; /* vendor control reg width */
+	u8      vendor_stat; /* vendor status reg witdh */
+	u8      dma_req_chan; /* bitmask for required dma channels */
+	u8      ram_bits;   /* ram address size */
+
+	struct musb_hdrc_eps_bits *eps_bits;
 };
--- 100_tmp/2.py
+++ 100_tmp/2.py
@@ -46,17 +46,28 @@ def process_member(line):
     s_value = line[i+1:]
     return '"' + s_type + '"' + ' : ' + '"' + s_value + '"'
 
-# "int     (*set_vbus)(struct device *dev, int is_on);"
+# "int     (*set_vbus)(struct device *dev, int is_on)"
 def process_func(line):
     i = line.find(')(')
     s_f = line[0:i+1]
     s_f = '"' + s_f + '"'
 
-    s_parameter1 = line[i+2: -2]   # (struct device *dev, int is_on);
+    s_parameter1 = line[i+2: -1]   # (struct device *dev, int is_on)
     parameters = ''
     ls = s_parameter1.split(',')
     for item in ls:
-        para = process_member(item)
+        #para = process_member(item)
+        #i = line.rfind(" ")
+
+        # type 中不能有  “ ” ， 空格隔开的多个字符串 因此只取最后一个 
+        i = item.rfind(" ")
+        s_type = item[:i].strip()
+        ls_type = s_type.split(' ')
+        s_type = ls_type[-1]
+
+        s_value = item[i+1:]
+        para = s_type + ':' + '"' + s_value + '"'
+
         parameters += para + ' , '
 
     parameters = parameters[:-3]  # 去掉最后一个 ' , '
@@ -67,6 +78,25 @@ def process_func(line):
 
     return s_f + parameters
 
+
+
+def process_line(line, m_flag, m_lines):
+    i = line.find(";")
+    if i != -1:
+        if m_flag != 0:
+            m_flag = 0
+            line = m_lines + line
+            i = line.find(";")
+
+        line = line[:i]
+        return line, m_flag, m_lines
+    else:
+        print("mulit line")
+        m_flag = 1
+        m_lines += line
+        return -1, m_flag, m_lines
+
+        
 
 if __name__ == '__main__':
     import numpy as np
@@ -80,32 +110,49 @@ if __name__ == '__main__':
     # 全局变量
     text = ""
     class_t = ""
-    struct_begin  = 1
+    struct_begin  = 0
+    m_flag = 0
+    m_lines = ''
+
+    lcommit_start = 0
 
     
     
     for line in in_lines:
+        #print('source line = \n', line)
 
         # 1.预处理 -----------------------------
-        if line.startswith("//") or \
-            line.startswith("/*") or \
-            line.startswith("*")  or \
-            line.startswith("*/") or \
-            line.startswith("#define") or\
-            line == "":
-
+        if line.startswith("//") or line == "":
+            #line.startswith("/*") or \
+            #line.startswith("*")  or \
+            #line.startswith("*/") or \
+            #line.startswith("#define") or\
+            
             continue
 
         line = line.strip()
         if line.find("//") != -1:
             line = line[0:line.find("//")]
-        if line.find("/*") != -1:
+        if line.find("/*") != -1 and line.find("*/") != -1:    #  在同一行
             line = line[0:line.find("/*")]
-            
-        print("\n--0. 1.预处理 line = \n", line)
 
+        
+        # 跨行注释
+        if line.find("/*") != -1:
+            lcommit_start = 1
+            continue
+
+        if line.find("*/") != -1:
+            lcommit_start = 0
+            continue
+
+        if lcommit_start == 1:
+            continue
+            
+        print("\n\n----1.通过预处理的 line = \n", line)
+        #sys.exit(0)
         # 2. 是 struct开始
-        if line.startswith("struct"):
+        if line.startswith("struct") and struct_begin == 0:
             struct_begin = 1
             line = line.replace("struct", "class")
             class_t = line + '\n'
@@ -116,35 +163,42 @@ if __name__ == '__main__':
         if line == "};":
             struct_begin = 0
             class_t += "}\n"
-            text += class_t
+            text += '\n' + class_t
             class_t = ''
             print('\n 3. struct结束')
             continue
 
         # 4. 判断是成员还是函数
         if struct_begin:
-            print('\n--4. struct_begin')
+            print('\n--4. in struct, line = ', line)
             line = strip_commit(line)
 
-            i = line.find(";")
-            if i == -1:
-                print("not a c statement")
+            line, m_flag, m_lines = process_line(line, m_flag, m_lines)
+            if line == -1:
                 continue
-            else:
-                line = line[:i]
-
+            
+            m_lines = ''
+            #print('----line = ----------------', line)
             i = line.find(r")(")
-            print(i)
             if i == -1:
                 # 4.1 处理line成员
                 member = process_member(line)
                 print('member = ', member)
-                class_t += member + '\n'
+                class_t += '\t' + member + '\n'
 
             else:
                 # 4.2 处理line方法
                 func = process_func(line)
                 print('func = ', func)
-                class_t += func + '\n'
+                class_t += '\t' + func + '\n'
 
-    print(text)
+    print('\ntext =\n', text)
+
+
+    # 5. 处理 ClassDiagram
+    print('\n --------5. 处理 ClassDiagram')
+    text = "ClassDiagram {\n\n" + text + '\n\n}'
+    dir_out = "2_out.dotuml"
+    f_out = open(dir_out, 'w', encoding='utf-8')
+    f_out.write(text)
+    f_out.close()
